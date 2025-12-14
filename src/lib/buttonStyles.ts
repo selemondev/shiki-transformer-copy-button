e879bbec89f7c44eabf7c0bb8f849f0ba823d501
selemondev <selemondev19@gmail.com>
2025-12-13 20:37:28 -0700

    fix(style): prevent Shiki override

--- src/lib/buttonStyles.ts
+++ src/lib/buttonStyles.ts
@@ -1,170 +1,117 @@
-// Inspired by rehype-pretty-code (https://github.com/rehype-pretty/rehype-pretty-code).
-// Modified by adding support for Shiki themes, CSS Variables and dark mode.
+// Inspired by rehype-pretty-code
+// Modified for Shiki + scoped copy button + optional dark mode
 
 export function buttonStyles({
-  successIcon = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3Cpath d='m9 14 2 2 4-4'/%3E%3C/svg%3E`,
-  copyIcon = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3C/svg%3E`,
+  successIcon = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3Cpath d='m9 14 2 2 4-4'/%3E%3C/svg%3E`,
+  copyIcon = successIcon,
   display,
-  cssVariables = `:root {
-      --border-color: #e2e2e3;
-      --background-color: #f6f6f7;
-      --hover-background-color: #ffff;
-      --dark-background-color: transparent;
-      --dark-border-color: #2e2e32;
-      --dark-background-color-hover: #1b1b1f;
-      --dark-border-color-hover: #2e2e32;
-      --button-top: 14px;
-      --button-right: 12px;
-      --button-z-index: 3;
-      --button-border-radius: 4px;
-      --button-width: 30px;
-      --button-height: 30px;
-      --button-ready-icon-width: 20px;
-      --button-ready-icon-height: 20px;
-      --button-success-icon-width: 20px;
-      --button-success-icon-height: 20px;
-      }`,
+  enableDarkMode = false,
+  cssVariables = `
+:root {
+  --button-border-color: #e2e2e3;
+  --button-bg: #f6f6f7;
+  --button-bg-hover: #ffffff;
+
+  --button-border-color-dark: #2e2e32;
+  --button-bg-dark: transparent;
+  --button-bg-hover-dark: #1b1b1f;
+
+  --button-top: 14px;
+  --button-right: 12px;
+  --button-z-index: 3;
+  --button-radius: 4px;
+  --button-size: 30px;
+  --icon-size: 20px;
+}
+`,
 }: {
   successIcon?: string
   copyIcon?: string
-  display?: string
+  display?: "hover" | "always"
+  enableDarkMode?: boolean
   cssVariables?: string
 }) {
-  let buttonStyle = `${cssVariables}
-      pre:has(code) {
-        position: relative;
-        overflow: hidden;
-      }
+  let styles = `
+${cssVariables}
 
-      pre:has(code) > code {
-        overflow-x: auto;
-        display: block;
-      }
+pre:has(code) {
+  position: relative;
+}
 
-      pre:has(code) button.shiki-transformer-button-copy {
-        position: absolute;
-        top: var(--button-top);
-        right: var(--button-right);
-        z-index: var(--button-z-index);
-        border: 1px solid var(--border-color);
-        border-radius: var(--button-border-radius);
-        width: var(--button-width);
-        height: var(--button-height);
-        display: flex;
-        justify-content: center;
-        place-items: center;
-        background-color: var(--background-color);
-        cursor: pointer;
-        background-repeat: no-repeat;
-        transition: background .25s, opacity .25s;
-        opacity: 1;
-      }
+pre:has(code) > button.shiki-transformer-button-copy {
+  position: absolute;
+  top: var(--button-top);
+  right: var(--button-right);
+  z-index: var(--button-z-index);
 
-      pre:has(code) button.shiki-transformer-button-copy:hover {
-        background-color: var(--hover-background-color);
-      }
+  width: var(--button-size);
+  height: var(--button-size);
+  border-radius: var(--button-radius);
+  border: 1px solid var(--button-border-color);
 
-      pre:has(code) button.shiki-transformer-button-copy span {
-        width: 100%;
-        aspect-ratio: 1 / 1;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-      }
+  background-color: var(--button-bg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-      pre:has(code) button.shiki-transformer-button-copy .ready {
-        width: var(--button-ready-icon-width);
-        height: var(--button-ready-icon-height);
-        background-image: url("${copyIcon}");
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-      }
+  cursor: pointer;
+  transition: background-color .2s, opacity .2s;
+}
 
-      pre:has(code) button.shiki-transformer-button-copy .success {
-        display: none;
-        width: var(--button-success-icon-width);
-        height: var(--button-success-icon-height);
-        background-image: url("${successIcon}");
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-      }
+pre:has(code) > button.shiki-transformer-button-copy:hover {
+  background-color: var(--button-bg-hover);
+}
 
-      pre:has(code) button.shiki-transformer-button-copy.shiki-transformer-button-copied .success {
-        display: block;
-      }
+pre:has(code) > button.shiki-transformer-button-copy .ready,
+pre:has(code) > button.shiki-transformer-button-copy .success {
+  width: var(--icon-size);
+  height: var(--icon-size);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+}
 
-      pre:has(code) button.shiki-transformer-button-copy.shiki-transformer-button-copied .ready {
-        display: none;
-      }
+pre:has(code) > button.shiki-transformer-button-copy .ready {
+  background-image: url("${copyIcon}");
+}
 
-      @media screen and (prefers-color-scheme: dark) {
-        pre:has(code) button.shiki-transformer-button-copy {
-          background-color: var(--dark-background-color);
-          border: 1px solid var(--dark-border-color);
-        }
+pre:has(code) > button.shiki-transformer-button-copy .success {
+  display: none;
+  background-image: url("${successIcon}");
+}
 
-        pre:has(code) button.shiki-transformer-button-copy:hover {
-          background-color: var(--dark-background-color-hover);
-          border: 1px solid var(--dark-border-color-hover);
-        }
+pre:has(code) > button.shiki-transformer-button-copy.shiki-transformer-button-copied .ready {
+  display: none;
+}
 
-        .shiki, .shiki span {
-          color: var(--shiki-dark) !important;
-          background-color: var(--shiki-dark-bg) !important;
-          font-style: var(--shiki-dark-font-style) !important;
-          font-weight: var(--shiki-dark-font-weight) !important;
-          text-decoration: var(--shiki-dark-text-decoration) !important;
-        }
-      }
+pre:has(code) > button.shiki-transformer-button-copy.shiki-transformer-button-copied .success {
+  display: block;
+}
+`
 
-      .dark pre:has(code) button.shiki-transformer-button-copy {
-        background-color: var(--dark-background-color);
-        border: 1px solid var(--dark-border-color);
-      }
-
-      .dark pre:has(code) button.shiki-transformer-button-copy:hover {
-        background-color: var(--dark-background-color-hover);
-        border: 1px solid var(--dark-border-color-hover);
-      }
-
-      html.dark .shiki, html.dark .shiki span {
-        color: var(--shiki-dark) !important;
-        background-color: var(--shiki-dark-bg) !important;
-        font-style: var(--shiki-dark-font-style) !important;
-        font-weight: var(--shiki-dark-font-weight) !important;
-        text-decoration: var(--shiki-dark-text-decoration) !important;
-      }
-
-      html.dark pre:has(code) button.shiki-transformer-button-copy .ready,
-      html.dark pre:has(code) button.shiki-transformer-button-copy .success {
-        background-color: var(--dark-background-color) !important;
-      }
-
-      html.dark pre:has(code) button.shiki-transformer-button-copy:hover .ready,
-      html.dark pre:has(code) button.shiki-transformer-button-copy:hover .success {
-        background-color: var(--dark-background-color-hover) !important;
-      }
-    `
-
-  if (display === 'hover') {
-    buttonStyle += `
-      pre:has(code) button.shiki-transformer-button-copy {
-        opacity: 0;
-      }
-      pre:has(code):hover button.shiki-transformer-button-copy {
-        opacity: 1;
-      }
-    `
-  }
-  else {
-    buttonStyle += `
-      pre:has(code) button.shiki-transformer-button-copy {
-        opacity: 1;
-      }
-    `
+  if (display === "hover") {
+    styles += `
+pre:has(code) > button.shiki-transformer-button-copy {
+  opacity: 0;
+}
+pre:has(code):hover > button.shiki-transformer-button-copy {
+  opacity: 1;
+}
+`
   }
 
-  return buttonStyle
+  if (enableDarkMode) {
+    styles += `
+html.dark pre:has(code) > button.shiki-transformer-button-copy {
+  background-color: var(--button-bg-dark);
+  border-color: var(--button-border-color-dark);
+}
+
+html.dark pre:has(code) > button.shiki-transformer-button-copy:hover {
+  background-color: var(--button-bg-hover-dark);
+}
+`
+  }
+
+  return styles
 }
